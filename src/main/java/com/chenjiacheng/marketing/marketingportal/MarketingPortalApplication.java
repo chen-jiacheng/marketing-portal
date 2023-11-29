440e4ad9f826ef4ec91f5bc64e0cd8e3d87c6154
chenjiacheng <chen-jiacheng@foxmail.com>
2023-11-29 23:24:26 +0800

    # first commit

---
+++ src/main/java/com/chenjiacheng/marketing/marketingportal/MarketingPortalApplication.java
@@ -0,0 +1,13 @@
+package com.chenjiacheng.marketing.marketingportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketingPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chenjiacheng/marketing/marketingportal/MarketingPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenjiacheng.marketing.marketingportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketingPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
